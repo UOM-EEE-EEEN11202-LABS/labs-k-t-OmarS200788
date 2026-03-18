bec422e99c11846a0200628822b1be985abe8063
OmarS200788 <omar.shehadeh@student.manchester.ac.uk>
2026-03-18 10:25:21 +0000

    lab L done

--- lab-k/assignment_k.c
+++ lab-k/assignment_k.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-
+#include <math.h>
+//declaring variables
 int main(void) {
   double height = 0;
+  double volume;
+  double radius = 2.2;
+  char email[] = "omar.shehadeh@student.manchester.ac.uk";
+  
   printf("Enter height: ");
   scanf("%lf", &height);
 
   // Your code in this section
+  //height 
+  if (height < 0){
+    printf("Error");
+    return 1 ;
+  }
+  //calculation
+  volume = height * M_PI * radius*radius;
+
 
   // Do not edit below here
   printf("%s\n", email);
--- lab-l/assignment_l.c
+++ lab-l/assignment_l.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
+  char my_email[] = "omar.shehadeh@student.manchester.ac.uk";
   int n;
   printf("Enter an integer: ");
   scanf("%d", &n);
 
   // Your code in this section
+  while (n<0 || n>38){
+    printf("Your response must be between 0 and 38.");
+    scanf("%d", &n);
+  }
+
+  if (my_email[n]=='a'|| my_email[n] == 'e' || my_email[n] == 'i' || my_email[n] == 'o' || my_email[n] == 'u') {
+    printf("Yes!");
+  }
+  else{ 
+      printf("No!"); }
 
 
   printf("Yes!"); // place as needed in the code. You can use more than once.
@@ -14,4 +25,4 @@ int main(void) {
 
   // Do not edit below here
   return 0;
-}
+  }
